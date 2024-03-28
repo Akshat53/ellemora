2a86f213f11ea021810ba4163a2bd155e7c0255b
shambhavits16 <shambhavits16@gmail.com>
2024-03-28 14:25:42 +0530

    updated

--- client/src/components/HeroSection/HeroSection.tsx
+++ client/src/components/HeroSection/HeroSection.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, Image } from "antd";
+import { Row, Col } from "antd";
 import Styles from "./hero.module.css";
 
 const HeroSection: React.FC = () => {
@@ -9,7 +9,7 @@ const HeroSection: React.FC = () => {
         <div
           className={`${Styles.overlayBox} d-flex h-100 w-100 align-items-end justify-content-center`}
         >
-          <p className="text-light heading">dschubdsxcsd</p>
+          <p className="text-light heading">TAILORED DEISGNING JUST FOR YOU</p>
         </div>
       </Col>
     </Row>
