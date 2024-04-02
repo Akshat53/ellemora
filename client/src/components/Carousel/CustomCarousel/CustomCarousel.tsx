b26647e985839b5f82aa6a3f0b5140c9d97bbf01
Akshat singh <singh.akshat.akshat26@gmail.com>
2024-04-02 20:11:24 +0530

    updated

--- client/src/components/Carousel/CustomCarousel/CustomCarousel.tsx
+++ client/src/components/Carousel/CustomCarousel/CustomCarousel.tsx
@@ -1,41 +1,60 @@
-import "./custom.module.css";
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Slider from "react-slick";
 
-interface customCarouselProps {
+interface homeCarouselProps {
   data: {
     img: string;
     text: string;
-    link : string
+    link: string;
   };
 }
 
-const CustomCarousel: React.FC<customCarouselProps> = ({ data }) => {
+const CustomCarousel: React.FC<homeCarouselProps> = ({ data }) => {
   const [imageIndex, setImageIndex] = useState(0);
-  const [isActive,setIsActive] = useState(false);
-
   const settings = {
-    centerMode: false,
-    centerPadding: "20%",
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    draggable: true,
-    focusOnSelect: true,
-    
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
-
   return (
-    <div className="w-100">
+    <div className="slider-container">
       <Slider {...settings}>
         {data.map((item, idx) => (
-          <Link to={idx==imageIndex ? item.link: isActive} key={idx} className="text-decoration-none">
-          <div
-            className={
-              idx === imageIndex ? "slide activeSlide" : "slide slideImg"}>
-            <img src={item.img} alt={item.text} />
-            <p className="text-center">{item.text}</p>
-          </div>
+          <Link to={"/"} key={idx} className="text-decoration-none">
+            <div>
+              <img src={item.img} alt={item.text} />
+              <p className="text-center">{item.text}</p>
+            </div>
           </Link>
         ))}
       </Slider>
